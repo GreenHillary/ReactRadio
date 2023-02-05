1374e721b163dc724e654d26ba28ec91bc7a32da
GreenHillary <79810984+GreenHillary@users.noreply.github.com>
2023-02-05 12:49:55 -0600

    Styling updates to accordion, color changing background behind player

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -2,16 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import PlayerComponent from '../PlayerComponent/PlayerComponent';
 
 function NavigationBar(props) {
   return (
     <>
       {[false].map((expand) => (
-        <Navbar key={expand} bg="light" expand={expand} className="shadow" sticky="top">
+        <Navbar key={expand} bg="light" expand={expand} className="shadow" sticky="fixed">
           <Container fluid>
-            <Navbar.Brand href="#">KUTX Radio{props.show && ' - ' + props.show}{props.DJName && ' - ' + props.DJName}</Navbar.Brand>
-            <span><PlayerComponent /></span>
+            <Navbar.Brand href="#">KUTX Radio</Navbar.Brand>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
@@ -22,12 +20,10 @@ function NavigationBar(props) {
                 <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
                   KUTX
                 </Offcanvas.Title>
-                <a href={props.showUrl} target="_blank" rel="noreferrer">{props.show}</a>
-                <a href={props.showUrl} target="_blank" rel="noreferrer">{props.DJName}</a>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3 text-center w-75 mx-auto">
-                  <h2>Now Playing</h2>
+                  <h4>Now Playing</h4>
                   {props.currentTrack.artworkUrl100
                     ? <img src={props.currentTrack.artworkUrl100} alt="song cover" width="100" className='mx-auto' />
                     : <img src="https://place-puppy.com/100x100" alt="song cover" width="100" className='mx-auto' />
